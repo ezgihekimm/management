335eda436ce15c919209d72b579618657257855b
ezgi.hekim.2000 <ezgi.hekim.2000@gmail.com>
2024-08-07 12:48:09 +0300

    done sidebar

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -2,7 +2,13 @@
 import Image from 'next/image'
 import { useState } from 'react'
 
-function DropDown() {
+interface DropDownProps {
+  title: string
+  dotColor: string
+}
+
+function DropDown(props: DropDownProps) {
+  const { dotColor, title } = props
   const [isOpen, setIsOpen] = useState(false)
 
   const toggleDropdown = () => {
@@ -13,11 +19,14 @@ function DropDown() {
     <div className="relative inline-block w-[214px]">
       <div
         onClick={toggleDropdown}
-        className="mb-2 flex cursor-pointer flex-row items-center justify-between rounded-[6px] px-3 py-2 hover:bg-[#F9FAFB]"
+        className="mb-2 flex cursor-pointer flex-row items-center justify-between rounded-[6px] px-3 py-2 text-[#344054] hover:bg-[#F9FAFB] hover:text-[#101828] active:bg-[#F3F6FD] active:font-semibold active:text-[#145389]"
       >
         <div className="flex flex-1 flex-row items-center">
-          <div className="h-2.5 w-2.5 rounded-full bg-[#2083d7]"></div>
-          <div className="pl-3 text-sm text-[#344054]">Proje İsim 1</div>
+          <div
+            className="h-2.5 w-2.5 rounded-full"
+            style={{ backgroundColor: dotColor }}
+          ></div>
+          <div className="pl-3 text-sm">{title}</div>
         </div>
 
         <button onClick={toggleDropdown} className="pl-2">
@@ -31,7 +40,7 @@ function DropDown() {
       </div>
 
       {isOpen && (
-        <div className="absolute w-[226px] rounded-md">
+        <div className="mb-3 block w-[226px] rounded-md">
           <div className="flex flex-col gap-2 pr-3 text-xs">
             <div className="flex flex-row items-center justify-between rounded-[6px] pl-12 pr-3 hover:bg-[#F9FAFB]">
               <a href="#" className="block py-2 text-[#344054]">
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,36 @@
+import Image from 'next/image'
 import DropDown from './DropDown'
 
 function SideBar() {
   return (
-    <div className="flex flex-1 flex-col px-4 pt-9">
-      <div className="pb-4 text-base font-medium text-[#101828]">Projeler</div>
-      <DropDown />
+    <div className="flex flex-1 flex-col justify-between border-r-[1px] border-[#eaecf0] px-4 pt-9">
+      <div className="flex flex-col">
+        <div className="pb-4 text-base font-medium text-[#101828]">
+          Projeler
+        </div>
+        <DropDown title="Project 1" dotColor="#f04437" />
+        <DropDown title="Project 2" dotColor="#2083d7" />
+        <DropDown title="Project 3" dotColor="#fec84a" />
+        <DropDown title="Project 4" dotColor="#7e56d8" />
+        <div className="mt-2 flex flex-row px-3 py-2 text-[#98a1b2] hover:bg-[#F9FAFB] hover:text-[#101828]">
+          <Image src="/assets/bar-chart.svg" alt="" width={20} height={20} />
+          <div className="pl-2 text-sm">Proje Oluştur</div>
+        </div>
+      </div>
+      <div className="flex flex-row items-start justify-between pb-6 ps-1">
+        <div className="flex flex-col">
+          <div className="text-[#101828 ]text-sm font-semibold">
+            Olivia Rhye
+          </div>
+          <a
+            href="mailto:olivia@untitledui.com"
+            className="text-sm text-[#475467]"
+          >
+            olivia@untitledui.com
+          </a>
+        </div>
+        <button className="rounded-full border-2 border-[#475467] p-2"></button>
+      </div>
     </div>
   )
 }
